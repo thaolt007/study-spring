b931fc4d840374b03ed46a3e69ae6d02dde964a8
kien.lovan <kien.lovan@nccsoft.vn>
2018-04-02 14:20:01 +0700

    first commit

---
+++ src/com/spring/HelloWorld.java
@@ -0,0 +1,19 @@
+package com.spring;
+
+public class HelloWorld {
+	private String message;
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public void getMessage() {
+		System.out.println("Your Message : " + message);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/com/spring/MainApp.java
@@ -0,0 +1,14 @@
+package com.spring;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+		HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
+	}
+
+}
